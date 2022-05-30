39a1fdc8bf3616ccc3003df8ab746345447d7af4
T.Y.Kuan <tingyoungkuan@gmail.com>
2022-05-30 15:07:48 +0800

    Initial Commit

---
+++ Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by tingyang on 2022/5/30.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
